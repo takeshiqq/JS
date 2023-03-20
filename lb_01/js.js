03e86974b507265bf0e73774070cfdeed93c117f
takeshiq <newtakeshinoapex@gmail.com>
2023-03-20 19:00:27 +0200

    lb_01

---
+++ lb_01/js.js
@@ -0,0 +1,29 @@
+// 01
+// const birthYear = +prompt('Enter your birth year -> ', '');
+// let currentData = 2023;
+// let age;
+
+// alert(`Your age is -> ${currentData - birthYear}`);
+
+// 02
+// let radius = +prompt('Enter radius of a circle -> ', '');
+
+// alert(`Radius of a circle -> ${(radius * radius) * Math.PI}`);
+
+// 03
+// let distance = +prompt('Enter km distance between two cities -> ', '');
+// let time = +prompt('Enter the amount of time to reach the city two -> ', '');
+
+// alert(`speed they to arrive on time -> ${distance / time} km/hour`); 
+
+// 04
+// let dollars = +prompt('Enter dollars -> ', '');
+
+// alert(`dollars -> euros = ${dollars * 0.95}`);
+
+
+// 05
+// let flashDrive = +prompt('Enter the capacity of a flash drive in GB -> ', '');
+// let file = 820;
+
+// alert(`amount of files that can be saved on the flash drive -> ${(flashDrive * 1024) / file}`);
